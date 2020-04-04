2792e9f83aa646d2565fa8fded752a0e3e90f399
Jules Fouchy <jules.fouchy@ntymail.com>
2020-04-04 21:10:20 +0200

    Adapt to Heroku

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const serv = require('http').Server(app)
 app.get('/', (req, res) => res.sendFile(__dirname + '/client/index.html'))
 app.use('/client', express.static(__dirname + '/client'))
 
-serv.listen(2000, () => console.log('Server started.'))
+serv.listen(process.env.PORT || 2000, () => console.log('Server started.'))
 
 const io = require('socket.io')(serv,{})
 
