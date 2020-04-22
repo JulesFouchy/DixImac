a4cb47d8e9581e0b411907b1a7b579b87fc05a55
Jules Fouchy <jules.fouchy@ntymail.com>
2020-04-22 11:07:37 +0200

    display:none the canvas created by p5 ; add a footer and clean up the index.html

--- client/imageFromP5.js
+++ client/imageFromP5.js
@@ -3,6 +3,7 @@ let oCanvas
 function setup() {
 	createCanvas(500, 750)
 	oCanvas = document.getElementById("defaultCanvas0")
+	oCanvas.style = "display: none;"
 }
 
 imageFromP5script = (scriptStr, seed) => {
