75f326253bf228cb8c9e164b1173cd1750ae9313
Jules Fouchy <jules.fouchy@ntymail.com>
2020-05-06 11:56:59 +0200

    API : working cardSrc/:id request

--- api/api.js
+++ api/api.js
@@ -3,24 +3,44 @@ const router = express.Router()
 require('dotenv/config')
 
 const MongoClient = require('mongodb').MongoClient
+const ObjectId = require('mongodb').ObjectID
 const client = new MongoClient(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
-try {
-    client.connect().then( (client) => {
-        const db = client.db('diximac')
-        db.collection('authors').find().toArray( (err, result) => {
-            if (err) console.log(err)
+
+const dbRequest = (req) => {
+    try {
+        client.connect().then( (client) => {
+            const db = client.db('diximac')
+            req(db)
+        })
+    }
+    catch(err) {
+        console.log('-----Error while connecting to database-----')
+        console.log(err)
+        console.log('-------------------------')
+    }
+}
+
+dbRequest( db => {
+    db.collection('authors').find().toArray( (err, result) => {
+        if (err) console.log(err)
+        else {
+            console.log(result);
+        }
+    })
+})
+
+router.get('/cardSrc/:id', (req, res) => {
+    dbRequest( db => {
+        db.collection('cards').findOne({"_id": ObjectId(req.params.id)}, (err, result) => {
+            if (err) {
+                console.log('ERR')
+                res.send(err)
+            }
             else {
-                console.log(result);
+                res.send(result)
             }
         })
     })
-}
-catch(err) {
-    console.log('-----Error while connecting to database-----')
-    console.log(err)
-    console.log('-------------------------')
-}
-
-router.get('/cardSrc/:id', (req, res) => res.send(`Seen ${req.params.id}`))
+})
 
 module.exports = router
