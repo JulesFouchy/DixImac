8c275feb0d6cb2ad742e402d75b7d553089bb634
Jules Fouchy <jules.fouchy@ntymail.com>
2020-07-13 18:54:47 +0200

    Actually choose the bg according to daytime

--- client/set-bg-daytime.js
+++ client/set-bg-daytime.js
@@ -1,10 +1,12 @@
 const setBgDaytime = (containerName, bWithBoat) => {
     const el = document.getElementsByClassName(containerName)[0]
-    console.log("gfyrd")
-    if (bWithBoat) {
-        el.classList.add("bg-night-boat")
-    }
-    else {
-        el.classList.add("bg-dawn")
-    }
+    const hour = new Date().getHours()
+    if (hour >= 22 || hour <= 4)
+        el.classList.add(bWithBoat ? "bg-night-boat" : "bg-night")
+    else if (hour <= 8)
+        el.classList.add(bWithBoat ? "bg-dawn-boat" : "bg-dawn")
+    else if (hour <= 19)
+        el.classList.add(bWithBoat ? "bg-day-boat" : "bg-day")
+    else
+        el.classList.add(bWithBoat ? "bg-dusk-boat" : "bg-dusk")
 }
